139f00720023a37d9f8905b3bb7ede96281df8f9
Ilya Nesmiynov <ilya.nesmiyanov@chulakov.ru>
2020-01-10 18:11:45 +0300

    small changes in the components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,7 +5,7 @@ import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
   <div
-    className={`${size} menu-item`}
+    className={`${size ? size : ""} menu-item`}
     onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
     <div
