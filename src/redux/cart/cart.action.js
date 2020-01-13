c2e4b1f955c22c13dd3d48ce947a8155594b3c5b
Ilya Nesmiynov <ilya.nesmiyanov@chulakov.ru>
2020-01-13 16:36:45 +0300

    implemented add item and remove item functionality

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -8,3 +8,12 @@ export const addItem = item => ({
   type: types.ADD_ITEM,
   payload: item
 });
+
+export const removeItem = item => ({
+  type: types.REMOVE_ITEM,
+  payload: item
+});
+export const clearItem = item => ({
+  type: types.CLEAR_ITEM,
+  payload: item
+});
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,3 +11,19 @@ export const addItemToCart = (cartItems, cartToAdd) => {
 
   return [...cartItems, { ...cartToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartToRemove) => {
+  const cartIsExist = cartItems.find(
+    cartItem => cartItem.id === cartToRemove.id
+  );
+
+  if (cartIsExist.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartToRemove.id
+      ? { ...cartItem, quantity: cartToRemove.quantity - 1 }
+      : cartItem
+  );
+};
